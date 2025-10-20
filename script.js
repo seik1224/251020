cee5af6fc8ca0548a227970a84caecb6126aaedb
SBS세익 <seik1224@naver.com>
2025-10-20 19:13:36 +0900

    .

--- script.js
+++ script.js
@@ -33,3 +33,35 @@
 
 	완성되었다면 html에서 이상해씨, 파이리, 꼬부기 레벨을 바꿔보면서 제대로 작동하는지 확인해보세요~
 */
+
+
+var level1 = document.getElementById("level1");
+var level2 = document.getElementById("level2");
+var level3 = document.getElementById("level3");
+
+if(parseInt(level1.innerHTML) >= 10) {
+	document.getElementById('name1').innerHTML = "이상해풀"
+	document.getElementById("cha1").src = 'icon01_2.png'
+}
+if(parseInt(level1.innerHTML) >= 20) {
+	document.getElementById('name1').innerHTML = "이상해꽃"
+	document.getElementById("cha1").src = 'icon01_3.png'
+}
+
+if(parseInt(level2.innerHTML) >= 10) {
+	document.getElementById('name2').innerHTML = "리자드"
+	document.getElementById("cha2").src = 'icon02_2.png'
+}
+if(parseInt(level2.innerHTML) >= 20) {
+	document.getElementById('name2').innerHTML = "리자몽"
+	document.getElementById("cha2").src = 'icon02_3.png'
+}
+
+if(parseInt(level3.innerHTML) >= 10) {
+	document.getElementById('name3').innerHTML = "어니부기"
+	document.getElementById("cha3").src = 'icon03_2.png'
+}
+if(parseInt(level3.innerHTML) >= 20) {
+	document.getElementById('name3').innerHTML = "거북왕"
+	document.getElementById("cha3").src = 'icon03_3.png'
+}
